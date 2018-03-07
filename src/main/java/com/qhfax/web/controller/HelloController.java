08c5fb8260d6a9d8d8855dede7fb76278a449cdf
liujian496 <liujian496@163.com>
2018-03-07 16:43:54 +0800

    first commit

---
+++ src/main/java/com/qhfax/web/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.qhfax.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return ("Hello, qhfax-web");
+    }
+}
