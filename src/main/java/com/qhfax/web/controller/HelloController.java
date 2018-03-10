3075d43cc59900ca5ef680e9e73b954afd74a438
liujian496 <liujian496@163.com>
2018-03-10 11:48:52 +0800

    submit all file

--- src/main/java/com/qhfax/web/controller/HelloController.java
+++ src/main/java/com/qhfax/web/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.qhfax.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${qhfax.app.name}")
+    private String appName;
+
     @RequestMapping("/hello")
     public String sayHello(){
-        return ("Hello, qhfax-web-3");
+        String str = "Hello, qhfax-web-3, appname=[" + appName + "]";
+        return str;
     }
 }
