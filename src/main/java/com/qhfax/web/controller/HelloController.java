b8509917c9ed31146aeee8f92e9052ac33d69030
liujian496 <liujian496@163.com>
2018-03-10 10:02:57 +0800

    submit all file

--- src/main/java/com/qhfax/web/controller/HelloController.java
+++ src/main/java/com/qhfax/web/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String sayHello(){
-        return ("Hello, qhfax-web2");
+        return ("Hello, qhfax-web-3");
     }
 }
